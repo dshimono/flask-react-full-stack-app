f0348abf5013204a7a30e9f16d9eab83592d80f9
Denis Shimono <dshimono@gmail.com>
2024-05-09 16:52:08 +0100

    update url for backend

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -17,7 +17,7 @@ const ContactForm = ({ existingContact = {}, updateCallback }) => {
         };
 
         const url =
-            "http://127.0.0.1:5000/" +
+            "https://flask-react-contacts-app.onrender.com:80" +
             (updating
                 ? "update_contact/${existingContact.id}"
                 : "create_contact");
