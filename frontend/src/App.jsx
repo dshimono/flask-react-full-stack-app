1e86b10405ac000c9e014e6779b72805222e5e81
Denis Shimono <dshimono@gmail.com>
2024-05-09 10:36:02 +0100

    add tab with 4 spaces formating

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,62 +4,62 @@ import "./App.css";
 import ContactForm from "./ContactForm";
 
 function App() {
-  const [contacts, setContacts] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [currentContact, setCurrentContact] = useState({});
+    const [contacts, setContacts] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentContact, setCurrentContact] = useState({});
 
-  useEffect(() => {
-    fetchContacts();
-  }, []);
+    useEffect(() => {
+        fetchContacts();
+    }, []);
 
-  const fetchContacts = async () => {
-    const response = await fetch("http://127.0.0.1:5000/contacts");
-    const data = await response.json();
-    setContacts(data.contacts);
-  };
+    const fetchContacts = async () => {
+        const response = await fetch("http://127.0.0.1:5000/contacts");
+        const data = await response.json();
+        setContacts(data.contacts);
+    };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setCurrentContact({});
-  };
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setCurrentContact({});
+    };
 
-  const openCreateModel = () => {
-    if (!isModalOpen) setIsModalOpen(true);
-  };
+    const openCreateModel = () => {
+        if (!isModalOpen) setIsModalOpen(true);
+    };
 
-  const openEditModal = (contact) => {
-    if (isModalOpen) return;
-    setCurrentContact(contact);
-    setIsModalOpen(true);
-  };
+    const openEditModal = (contact) => {
+        if (isModalOpen) return;
+        setCurrentContact(contact);
+        setIsModalOpen(true);
+    };
 
-  const onUpdate = () => {
-    closeModal();
-    fetchContacts();
-  };
-  return (
-    <>
-      <Contactlist
-        contacts={contacts}
-        updateContact={openEditModal}
-        updateCallback={onUpdate}
-      />
-      <button onClick={openCreateModel}>Create New Contact</button>
-      {isModalOpen && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={closeModal}>
-              &times;
-            </span>
-            <ContactForm
-              existingContact={currentContact}
-              updateCallback={onUpdate}
+    const onUpdate = () => {
+        closeModal();
+        fetchContacts();
+    };
+    return (
+        <>
+            <Contactlist
+                contacts={contacts}
+                updateContact={openEditModal}
+                updateCallback={onUpdate}
             />
-          </div>
-        </div>
-      )}
-    </>
-  );
+            <button onClick={openCreateModel}>Create New Contact</button>
+            {isModalOpen && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <span className="close" onClick={closeModal}>
+                            &times;
+                        </span>
+                        <ContactForm
+                            existingContact={currentContact}
+                            updateCallback={onUpdate}
+                        />
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
 
 export default App;
