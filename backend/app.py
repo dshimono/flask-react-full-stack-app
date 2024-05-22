80a8eaad19264b3827a1f1ab216889de94358869
Denis Shimono <dshimono@gmail.com>
2024-05-22 19:01:02 +0100

    Revert "fix db connection"
    This reverts commit f48163c107b300b600959257a2807d8da8e3a717.

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ def create_app(db_url=None):
     load_dotenv()
     CORS(app)
     app.config["CORS_HEADERS"] = "Content-Type"
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = load_dotenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE EXECPTIONS"] = True
     app.config["API_TITLE"] = "Contacts List API"
@@ -25,14 +25,9 @@ def create_app(db_url=None):
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/api-documentation"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    db.init_app(app)
     migrate = Migrate(app, db)
 
     api = Api(app)
-    
-    @app.before_request
-    def create_tables():
-        db.create_all()
 
     api.register_blueprint(ContactBlueprint)
 
